316658c49e048d28b97303450feb30703c299c70
ivan <ivantjendra@gmail.com>
2021-09-29 20:57:16 +0800

    add hhtp.Handle for css main.go

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ func main() {
 		}
 	})
 
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("views"))))
 	fmt.Println("Listening on PORT 8080")
 	http.ListenAndServe(":8080", nil)
 }
